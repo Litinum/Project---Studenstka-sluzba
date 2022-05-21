aa728686cfebd9fca75af7823b1fd8a53b5f84dc
Litinum <73782133+Litinum@users.noreply.github.com>
2022-05-21 14:41:03 +0200

    Add files via upload

---
+++ src/main/java/poopprojekat/studentska_sluzba/StudentskaSluzbaApplication.java
@@ -0,0 +1,29 @@
+package poopprojekat.studentska_sluzba;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import poopprojekat.studentska_sluzba.Logger.MyLogger;
+
+@SpringBootApplication
+public class StudentskaSluzbaApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(StudentskaSluzbaApplication.class, args);
+        MyLogger logger = new MyLogger();
+        logger.start();
+        Database db = new Database("testing");
+        System.out.println(db);
+
+        Database.DropDatabase("testing");
+
+//        try
+//        {
+//            db.TestDummy();
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+    }
+
+}
